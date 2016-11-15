ba6d8f34aaef214199a4e9f44228de85c132ba31
Filipe11 <a68455@alunos.uminho.pt>
2016-11-15 16:33:48 +0000

    V1.2_14/11

---
+++ app/src/main/java/com/gateway/filiperodrigues/gatewayservice/ConectionAquisicaoDados.java
@@ -0,0 +1,33 @@
+package com.gateway.filiperodrigues.gatewayservice;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import org.json.JSONObject;
+
+public class ConectionAquisicaoDados extends AppCompatActivity {
+
+    JSONObject objectArray = new JSONObject();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_conection_aquisicao_dados);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
